59df7f1d0b092fcdfcd84483fe91de80f8a49e7d
dune211 <samuel.pintodasilva@students.hevs.ch>
2018-12-20 17:11:17 +0100

    Commit : new methods:
    - sendFile
    - downloadMethod
    - getRequestedFile
    - modif on createFileList

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,4 +1,7 @@
-import java.util.Vector;
+import javax.swing.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.net.*;
 import java.io.*;
@@ -23,7 +26,7 @@ public class Client {
     // main method
     public static void main(String[] args) {
         connexion(serverName, serverAddress, clientSocket, clientPort);
-        getClientFileList(clientFiles, filePath, localName, serverName, serverPort);
+        getClientFileList(clientFiles, filePath, localName, serverName);
     }
 
     // Method: Etablish the connection with the server
@@ -38,7 +41,6 @@ public class Client {
             System.out.println("We got the connexion to  " + sAddress);
             System.out.println("Will read data given by server:\n");
 
-
         } catch (UnknownHostException e) {
 
             e.printStackTrace();
@@ -52,29 +54,86 @@ public class Client {
     }
 
     // Method : create fileList for the server
-    public static ArrayList<FileIP> getClientFileList(ArrayList<FileIP> cFiles, String path, String cName, String sName, int sPort) {
+    public static ArrayList<FileIP> getClientFileList(ArrayList<FileIP> cFiles, String path, String cName, String sName) {
         File directory = new File(path);
 
         File[] files = directory.listFiles();
         for (int i = 0; i < files.length; i++) {
-            FileIP temp = new FileIP(files[i], localName); //create a temporary FileIP
+            FileIP temp = new FileIP(files[i], cName); //create a temporary FileIP
             cFiles.add(temp); // add it to the arrayList
         }
 
         return cFiles;
+    }
 
+    // Method: Accept incoming connection
+
+    // Method: Get the name of the requested file
+    public static String getRequestedFileName(Socket cSocket) throws IOException, ClassNotFoundException
+    {
+        ObjectInputStream ips = new ObjectInputStream(cSocket.getInputStream());
+        String fName = (String) ips.readObject();
+
+        return fName;
     }
 
     // Method: Send fileList to Server
 
 
-    // Method: Accept incoming connection : demander à Raph s'il comprend mieux que moi dans le code de Jon et Oli
+
+    // Method: Send file to client
+    public static boolean sendFile(Socket cSocket, String path, String fName) throws IOException
+    {
+        OutputStream ops = cSocket.getOutputStream();
+        String pathFile = path + fName;
+        Files.copy(Paths.get(pathFile), ops);
+
+        return true;
+    }
+
+
 
 
     // Method: Display list from server
+    /* 1: Créer la partie graphique (fenêtre, jpanel et jtable)
+       2: Récupérer la liste des fichiers que le serveur possède
+       3: L'afficher
+     */
 
 
     // Method: Download file (called from a listner)
+    public static boolean downloadFile(int cPort, FileIP fip, String path) throws IOException, ClassNotFoundException, FileAlreadyExistsException {
+        String fName = fip.getFile().getName();
+        String cName = fip.getIP();
+
+        InetAddress clientAddress = InetAddress.getByName(cName);
+        Socket clientSocket = new Socket();
+        try {
+            clientSocket.connect(new InetSocketAddress(clientAddress, cPort), 5);
+            InputStream inputStream = clientSocket.getInputStream();
+            ObjectOutputStream output = new ObjectOutputStream(clientSocket.getOutputStream());
+
+            //send requested file name
+            output.writeObject(fName);
+
+            //download requested file on path
+            try {
+                Files.copy(inputStream, Paths.get(path + fName));
+            } catch (Exception e) {
+                clientSocket.close();
+                String aeMessage = "File " + fName + " already exist"; // aeMessage = already exists message
+                JOptionPane.showMessageDialog(mainFrame, aeMessage);
+                return false;
+            }
+            clientSocket.close();
+        } catch (Exception e) {
+            String afkMessage = "The client " + cName + " is AFK";
+            JOptionPane.showMessageDialog(mainFrame, afkMessage);
+            return false;
+        }
+        return true;
+    }
+
 
 
 }
